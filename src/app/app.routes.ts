2afec1692997b28ad5f6937abe67bb33ea019f91
Sofia Ayala <sofiarayala1995@gmail.com>
2025-10-15 13:36:42 +0200

    Navbar routing added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { Map } from './features/map/map';
+import { About } from './features/about/about';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+       {
+    path: '',
+    children: [
+      { path: 'map', component: Map},
+      { path: 'about', component: About},
+      { path: '', component: Map},
+      
+    ],
+  },
+];
